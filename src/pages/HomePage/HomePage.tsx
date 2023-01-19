e6c6c54e246f27f5c9b8ef1239961841eaf83fa4
Adilxa <adilhanimerov82@gmail.com>
2023-01-20 00:16:16 +0600

    fix: conflicts

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,32 +5,6 @@ const HomePage = () => {
   return (
     <>
       <Main />
-      // just for test
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla minus
-        natus nostrum aut totam assumenda consectetur corrupti quo voluptatem
-        cupiditate distinctio enim eius id, quaerat nobis deserunt obcaecati,
-        odio aliquam, quam sit pariatur exercitationem vero! Vel sint ipsum
-        voluptates placeat consequatur nihil tenetur, voluptate eaque dolorum
-        suscipit harum, perferendis quos corrupti dicta. Ex adipisci impedit
-        eveniet aut ab voluptates earum unde fugit qui dolores commodi et iste
-        iusto natus rem odio cupiditate, placeat illo dignissimos praesentium
-        quia laudantium! Vel facere reprehenderit non asperiores deleniti
-        tenetur, adipisci incidunt quidem magnam, eius dolore id magni libero
-        aspernatur dignissimos laborum? Necessitatibus, voluptatum quidem sit ut
-        modi earum, maiores adipisci nesciunt officia cupiditate accusantium
-        magnam enim fuga, repudiandae molestias ducimus dolore deleniti. Tempore
-        magnam mollitia velit. Reiciendis corrupti nulla, enim ad ducimus sed
-        voluptate blanditiis itaque dicta officiis at distinctio. Repellendus
-        suscipit saepe cumque quos optio vero minus quam, odio aliquam expedita
-        et aliquid sit modi, consequuntur blanditiis nulla quia? Impedit,
-        aliquid eveniet autem hic obcaecati minima placeat perspiciatis
-        reprehenderit illum quae architecto facere unde? Architecto soluta error
-        natus quis cupiditate expedita dolorem numquam alias vitae mollitia
-        dignissimos libero repudiandae in, doloremque ut commodi molestiae
-        aliquid illo voluptate vel nesciunt culpa! Quia, cumque labore?
-      </p>
-      //
     </>
   );
 };
