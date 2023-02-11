11f76df05c2a8420ebc7687e200f8a659bb257a6
luchok22 <aitbaev2021@mail.ru>
2023-02-11 18:05:27 +0600

    feat: about us text block

---
+++ pages/aboutus.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AboutUsPage from '../src/pages/AboutUsPage/AboutUsPage';
+
+const AboutUs = () => {
+    return (
+        <div>
+            <AboutUsPage/>
+        </div>
+    );
+};
+
+export default AboutUs;
