d59e55d946ef2d35504627e0f27b9f7b746b7e5e
Serenbek <serenbek@mail.ru>
2023-02-17 19:01:14 +0600

    fix: cards shadow

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from "react";
 import scss from "./Header.module.scss";
 import Link from "next/link";
 import { HeaderLinks } from "../../constatnts/Main/HeaderConsts";
+import { useRouter } from "next/router";
 
 interface IHeader {
   isMain?: boolean;
 }
 
-const Header: React.FC<IHeader>= ({ isMain = false }) => {
+const Header: React.FC<IHeader> = ({ isMain = false }) => {
   const [isActive, setActive] = useState(false);
+  let activeLink = isActive ? scss.activeLink : "";
+  const router = useRouter();
+  const currentRoute = router.pathname;
 
   useEffect(() => {
     addEventListener("scroll", () => {
@@ -23,7 +27,11 @@ const Header: React.FC<IHeader>= ({ isMain = false }) => {
   const renderLinks = React.useMemo(
     () =>
       HeaderLinks.map((item) => (
-        <Link href={item.link} key={item.id}>
+        <Link
+          className={currentRoute == `/${item.link}` ? scss.activeLink : ""}
+          href={item.link}
+          key={item.id}
+        >
           {item.title}
         </Link>
       )),
@@ -38,7 +46,7 @@ const Header: React.FC<IHeader>= ({ isMain = false }) => {
 
   return (
     <header className={HeaderStyles}>
-      <Link href='/'>
+      <Link href="/">
         <h1>Logo</h1>
       </Link>
       <nav>{renderLinks}</nav>
