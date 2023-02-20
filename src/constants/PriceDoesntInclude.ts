8a5cb3cb0dbb357e535473c1d408175c544a8069
Almo <temirkanovalmanbet5@gmail.com>
2023-02-20 17:41:26 +0300

    feat: priceDoesntInclide

---
+++ src/constants/PriceDoesntInclude.ts
@@ -0,0 +1,17 @@
+interface IPriceDoesntInclude {
+  id: number;
+  img: string;
+  title: string;
+}
+export const PriceDoesntIncludeArr: IPriceDoesntInclude[] = [
+  {
+    id: 1,
+    img: "./images/PriceDoesntInclude/cross.svg",
+    title: "Hotel in Bishkek",
+  },
+  {
+    id: 2,
+    img: "./images/PriceDoesntInclude/cross.svg",
+    title: "Alcohol drinks",
+  },
+];
--- src/constants/TourFeatures.ts
+++ src/constants/TourFeatures.ts
@@ -1,43 +1,44 @@
-
 interface ITourFeatures {
-    id: number;
-    img: string;
-    title: string;
-};
+  id: number;
+  img: string;
+  title: string;
+}
 export const TourFeaturesArr: ITourFeatures[] = [
-    {
-        id: 1,
-        img: "./images/TourFeatures/checkMark.svg",
-        title: "Experience nomadic life in a traditional Kyrgyz boz-ui (yurt)"
-    },
-    {
-        id: 2,
-        img: "/images/TourFeatures/checkMark.svg",
-        title: "Learn about the history of the Silk Road visiting Burana Tower"
-    },
-    {
-        id: 3,
-        img: "./images/TourFeatures/checkMark.svg",
-        title: "Meet the Kyrgyz eagle hunters"
-    },
-    {
-        id: 4,
-        img: "./images/TourFeatures/checkMark.svg",
-        title: "Ride a horse in Song-Kol lake – best destination to combine horse riding and exploring nomad’s life"
-    },
-    {
-        id: 5,
-        img: "./images/TourFeatures/checkMark.svg",
-        title: "Hike to mountains and rivers in the Kyrgyzstan’s wilderness"
-    },
-    {
-        id: 6,
-        img: "./images/TourFeatures/checkMark.svg",
-        title: "Visit the most popular sites of the south shore of Ysyk-Kol Lake – Fairy-Tale canyons, Barskoon waterfalls and Jety-Oguz mountains."
-    },
-    {
-        id: 7,
-        img: "./images/TourFeatures/checkMark.svg",
-        title: "Explore the capital city – Bishkek on foot"
-    }
-]
+  {
+    id: 1,
+    img: "./images/TourFeatures/checkMark.svg",
+    title: "Experience nomadic life in a traditional Kyrgyz boz-ui (yurt)",
+  },
+  {
+    id: 2,
+    img: "/images/TourFeatures/checkMark.svg",
+    title: "Learn about the history of the Silk Road visiting Burana Tower",
+  },
+  {
+    id: 3,
+    img: "./images/TourFeatures/checkMark.svg",
+    title: "Meet the Kyrgyz eagle hunters",
+  },
+  {
+    id: 4,
+    img: "./images/TourFeatures/checkMark.svg",
+    title:
+      "Ride a horse in Song-Kol lake – best destination to combine horse riding and exploring nomad’s life",
+  },
+  {
+    id: 5,
+    img: "./images/TourFeatures/checkMark.svg",
+    title: "Hike to mountains and rivers in the Kyrgyzstan’s wilderness",
+  },
+  {
+    id: 6,
+    img: "./images/TourFeatures/checkMark.svg",
+    title:
+      "Visit the most popular sites of the south shore of Ysyk-Kol Lake – Fairy-Tale canyons, Barskoon waterfalls and Jety-Oguz mountains.",
+  },
+  {
+    id: 7,
+    img: "./images/TourFeatures/checkMark.svg",
+    title: "Explore the capital city – Bishkek on foot",
+  },
+];
