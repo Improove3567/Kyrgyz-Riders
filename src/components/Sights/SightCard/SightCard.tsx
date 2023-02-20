f406c4bb61b4e72a7d9a8ffc9b6b2e36e8fc54ee
Serenbek Ibraimkulov <serenbek@mail.ru>
2023-02-20 16:15:23 +0600

    fix:deleted borders

--- src/components/Sights/SightCard/SightCard.tsx
+++ src/components/Sights/SightCard/SightCard.tsx
@@ -7,30 +7,17 @@ const SightCard: React.FC<ISights> = ({
   img,
   title,
   tours,
-  isEdgeRight,
-  isEdgeLeft,
 }) => {
-  const isBack = (): string => {
-    if (isEdgeLeft) {
-      return "SightbackLeft";
-    } else if (isEdgeRight) {
-      return "SightBackRight";
-    } else {
-      return "SightBack";
-    }
-  };
-
 
   return (
     <div className={scss.card}>
-      <div className={isBack()}></div>
       <div className={scss.front}>
         {/* eslint-disable-next-line no-use-before-define */}
         <img src={img} alt="image" />
         {/* <Image src={img} alt="test" width={50} height={50} /> */}
         <div className={scss.btm}>
           <p>{title}</p>
-          <p>{tours.length} tours</p>
+          <p>{tours?.length} tours</p>
         </div>
       </div>
     </div>
