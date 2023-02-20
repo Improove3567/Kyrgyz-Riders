4f291cec83a521d74f6984573ad59dfc2090e80f
Almo <temirkanovalmanbet5@gmail.com>
2023-02-20 17:56:57 +0300

    feat/priceDoesntInclude

--- src/components/PriceDoesntIncInclude/PriceDoesntInclude.tsx
+++ src/components/PriceDoesntIncInclude/PriceDoesntInclude.tsx
@@ -3,13 +3,14 @@ import { PriceDoesntIncludeArr } from "../../constants/PriceDoesntInclude";
 import Divider from "../Divider/Divider";
 import scss from './PriceDoesntInclude.module.scss'
 import Image from "next/image";
+import cross from "../../../public/images/PriceDoesntInclude/cross.svg"
 const PriceDoesntInclude: FC = () => {
     const showText = React.useMemo(
         () =>
             PriceDoesntIncludeArr.map((item) => (
                 <div className={scss.paragraph} key={item.id}>
                     <div className={scss.title_mark}>
-                        <Image src={item.img} alt='dasd' width={16} height={12} />
+                        <Image src={cross} alt='dasd' width={16} height={12} />
                         <p>{item.title}</p>
                     </div>
                 </div>
