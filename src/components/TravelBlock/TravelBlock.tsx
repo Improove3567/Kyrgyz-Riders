fba3b245a5e0f1c620eb2dd43c5fcfedc80c2d6c
Adilxa <adilhanimerov82@gmail.com>
2023-03-20 19:23:51 +0600

    feat:video in travel

--- src/components/TravelBlock/TravelBlock.tsx
+++ src/components/TravelBlock/TravelBlock.tsx
@@ -1,25 +1,27 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import scss from "./TravelBlock.module.scss";
 import Divider from "../Divider/Divider";
 import { TravelData } from "../../constants/TravelData";
 import TravelCard from "./TravelCard/TravelCard";
 
 const TravelBlock: React.FC = () => {
+
     const sightsList = useMemo(() => (
         TravelData.map((el) => (
             <TravelCard {...el} key={el.date} />
         ))
-    ), [])
+    ), [TravelData])
+
     return (
         <div className={scss.wrapper}>
             <div className="container">
-            <Divider title={"Travel & Stories"} variant={"dark"} />
-            <div className={scss.card_container}>
-                {sightsList}
-            </div>
-            <div className={scss.button}>
-                <p>More Sights</p>
-            </div>
+                <Divider title={"Travel & Stories"} variant={"dark"} />
+                <div className={scss.card_container}>
+                    {sightsList}
+                </div>
+                <div className={scss.button}>
+                    <p>More Sights</p>
+                </div>
             </div>
         </div>
     );
--- src/components/TravelBlock/TravelCard/TravelCard.tsx
+++ src/components/TravelBlock/TravelCard/TravelCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import scss from "./TravelCard.module.scss";
 import Image from "next/image";
 
@@ -8,11 +8,23 @@ interface SightCardProps {
 }
 
 const TravelCard: React.FC<SightCardProps> = ({ date, title }) => {
+    const [isActive, setActive] = useState<boolean>(false);
+
+    const url = "https://www.youtube.com/embed/3KbUohmn7UE"
+
+    const renderVideo = useMemo(() => <iframe className={scss.video} width="560" height="315" src="https://www.youtube.com/embed/3KbUohmn7UE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>, [isActive, url])
+
     return (
-        <div className={scss.wrapper}>
-            <div className={scss.playBtn}>
-                <Image src="/images/travelStories/play.svg" width={20} height={20} alt="playBtn" />
-            </div>
+        <div className={isActive ? scss.active : scss.wrapper}>
+            {
+                isActive ? <div className={scss.active}>
+                    <Image src="/images/travelStories/close.svg" className={scss.close} onClick={() => setActive(false)} width={20} height={20} alt="close"></Image>
+                    {renderVideo}
+                </div>
+                    : <div className={scss.playBtn} onClick={() => setActive(true)}>
+                        <Image src="/images/travelStories/play.svg" width={20} height={20} alt="playBtn" />
+                    </div>
+            }
             <div className={scss.bottomBlock}>
                 <p className={scss.title}>{title}</p>
                 <p className={scss.date}>{date}</p>
