18112e1db9a371ef0ddf446894234bbf79d075e1
Serenbek Ibraimkulov <serenbek@mail.ru>
2023-03-27 13:47:20 +0600

    addArrowForSelTour

--- src/components/Tour slider/TourSLider.tsx
+++ src/components/Tour slider/TourSLider.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Divider from "../Divider/Divider";
 import FilterTour from "../Tour filter/FilterTour";
 import useTours from "../../hooks/useTours";
+import MoreBlock from "../Divider/More block/MoreBlock";
 
 interface ArrowProps {
   onClick: React.MouseEventHandler<HTMLDivElement>;
@@ -71,7 +72,12 @@ const TourSlider: React.FC = () => {
       <div className="container">
         <div className={scss.containerDivider}>
           <Divider title="Select Tour">
-            <FilterTour />
+            <>
+              <FilterTour />
+              <div className={scss.selectsArrow}>
+                <MoreBlock title="Select Tour" />
+              </div>
+            </>
           </Divider>
         </div>
         <div className="mainSliders">
