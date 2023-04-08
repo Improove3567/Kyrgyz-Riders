7eb21cb65e03781905300bfa059605d11ebd64aa
Serenbek Ibraimkulov <serenbek@mail.ru>
2023-04-08 17:10:41 +0600

    adaptiveOTDP

--- src/components/TeamDetailBlock/TeamDetailBlock.tsx
+++ src/components/TeamDetailBlock/TeamDetailBlock.tsx
@@ -40,8 +40,14 @@ const TeamDetailBlock: FC<TeamDetailProps> = () => {
                 <div className={scss.DImage}> 
                     <img src={teamDetail?.image} alt='detail image' width={384} height={384} /> 
                     <div className={scss.blocDPage}> 
-                        <div className={scss.divider}><Divider title={fullName} variant='dark' /></div> 
-                        <p className={scss.description}>{teamDetail?.description}</p> 
+                        <div className={scss.containerDivider}> 
+                            <div className={scss.divider}><Divider title={fullName} variant='dark' /></div> 
+                        </div>
+                        <div className={scss.containerForAdap}>
+                            <img src={teamDetail?.image} alt='detail image' width={384} height={384} /> 
+                            {/* <p className={scss.description}>{teamDetail?.description}</p>  */}
+                            <p className={scss.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eligendi quidem tempora sed. Distinctio soluta quae dolorum possimus dignissimos asperiores ipsam eos ipsa, tempore molestias mollitia maxime aliquid, nulla ullam quibusdam odit repellat sit illum rem fuga esse maiores non laudantium iste! Officia suscipit quas saepe, deleniti rem quidem culpa.</p>
+                        </div>
                     </div> 
                 </div> 
             </div> 
