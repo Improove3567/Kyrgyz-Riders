f25a8c692ab23ad923174a982a11fa64ef35f825
luchok22 <aitbaev2021@mail.ru>
2023-04-13 18:45:40 +0600

    fix: add dynamic route in sight detail page(tour)

--- src/components/SightTourBlock/SightTourBlock.tsx
+++ src/components/SightTourBlock/SightTourBlock.tsx
@@ -1,26 +1,18 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { useRouter } from "next/router";
-import useSights from "../../hooks/useSights";
+import React, { useMemo } from "react";
 import Divider from "../Divider/Divider";
 import MoreBlock from "../Divider/More block/MoreBlock";
 import scss from "./SightTourBlock.module.scss";
 import SightTourCard from "./SightTourCard/SightTourCard";
-import useTours from "../../hooks/useTours";
-import { SightTourArrProps } from "../../constants/SightTourBlock";
-import TourInfoTypes from "../../constants/SightTourBlock";
-
 
 interface TourProps {
   tours: Array<object>;
 }
 
 const SightTourBlock: React.FC<TourProps> = ({ tours }) => {
-
   const renderCards = useMemo(
     () => tours?.map((el, index) => <SightTourCard {...el} key={index} />),
     [tours]
   );
-
   return (
     <div className={scss.wrapper}>
       <div className="container">
